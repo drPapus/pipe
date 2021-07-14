609150e0fb34286ea302c2036dfff05ad4cf4a82
anton <antonklimovv@gmail.com>
2021-07-14 12:30:06 +0300

    tube with cube

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-//Get window size
-var ww = window.innerWidth,
-  wh = window.innerHeight;
+function main() {
+  const canvas = document.querySelector('#bg');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-//Create a WebGL renderer
-var renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas")
-});
-renderer.setSize(ww, wh);
+  const fov = 45;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 20);
 
-//Create an empty scene
-var scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0x000000,100,200);
+  const controls = new THREE.OrbitControls(camera, canvas);
+  controls.target.set(0, 0, 0);
+  controls.update();
 
-//Create a perpsective camera
-var camera = new THREE.PerspectiveCamera(45, ww / wh, 0.001, 200);
-camera.position.z = 400;
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('lightblue');
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 10, 0);
+    light.target.position.set(-5, 0, 0);
+    scene.add(light);
+    scene.add(light.target);
+  }
+
+  //Helpers
+
+  const gridHelper = new THREE.GridHelper(100, 10);
+  scene.add(gridHelper);
+  gridHelper.position.set(0, -5, 0);
+
+  const cube = new THREE.Mesh(
+     new THREE.BoxBufferGeometry(0.3, 0.3, 0.3),
+     new THREE.MeshPhongMaterial({color: 'red'}),
+  );
+  scene.add(cube);
+
 
 //Array of points
 var points = [
@@ -32,44 +55,92 @@ var points = [
 //Convert the array of points into vertices
 for (var i = 0; i < points.length; i++) {
   var x = points[i][0];
+  //We set a random value for the Y axis
   var y = (Math.random()-0.5)*250;
   var z = points[i][1];
   points[i] = new THREE.Vector3(x, y, z);
 }
 //Create a path from the points
-var path = new THREE.CatmullRomCurve3(points);
+let path = new THREE.CatmullRomCurve3(points);
 
-//Set colors
-var colors = [0xFF6138,0xFFFF9D,0xBEEB9F,0x79BD8F,0x00A388];
-//Loop through all those colors
-for(var i=0;i<colors.length;i++){
-  //Create a new geometry with a different radius
-  var geometry = new THREE.TubeBufferGeometry( path, 100, (i*2)+4, 10, true );
-  //Set a new material with a new color and a different opacity
-  var material = new THREE.MeshBasicMaterial({
-    color:colors[i],
-    transparent:true,
-    wireframe:true,
-    opacity : ((1- i/5)*0.5 + 0.1)
-  });
-  //Create a mesh
-  var tube = new THREE.Mesh( geometry, material );
-  //Push the mesh into the scene
-  scene.add( tube );
-}
+//Create the tube geometry from the path
+let geometry = new THREE.TubeGeometry( path, 300, 4, 32, true );
+//Basic material
 
-var percentage = 0;
-function render(){
+let tubeTexture = new THREE.MeshLambertMaterial({
+  color:0xf00fff,
+  side : THREE.BackSide
+});
+//Create a mesh
+var tube = new THREE.Mesh( geometry, tubeTexture );
+//Add tube into the scene
+scene.add( tube );
 
-  percentage += 0.0003;
-  var p1 = path.getPointAt(percentage%1);
-  var p2 = path.getPointAt((percentage + 0.01)%1);
-  camera.position.set(p1.x,p1.y,p1.z);
-  camera.lookAt(p2);
-  
-  //Render the scene
-  renderer.render(scene, camera);
+
+
+
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  let percentage = 0;
+
+  let then = 0;
+  function render(now) {
+    now *= 0.001; // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    
+    cube.rotation.x = now;
+    cube.rotation.y = now * 1.1;
+
+    percentage += 0.0003;
+    var p1 = path.getPointAt(percentage%1);
+    var p2 = path.getPointAt((percentage + 0.01)%1);
+    camera.position.set(p1.x,p1.y,p1.z);
+    camera.lookAt(p2);
+    
+    // move cube in front of camera
+    {
+      const distanceFromCamera = 3;  // 3 units
+      const target = new THREE.Vector3(0, 0, -distanceFromCamera);
+      target.applyMatrix4(camera.matrixWorld);    
+    
+      const moveSpeed = 35;  // units per second
+      const distance = cube.position.distanceTo(target);
+      if (distance > 0) {
+        const amount = Math.min(moveSpeed * deltaTime, distance) / distance;
+        cube.position.lerp(target, amount);
+        cube.material.color.set('green');
+      } else {
+        cube.material.color.set('red');
+      }
+    }
+
+    
+
+
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
 
   requestAnimationFrame(render);
 }
-requestAnimationFrame(render);
+
+main();
